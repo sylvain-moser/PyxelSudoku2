ab16d277d4ec2e8faf0702806832d1275dce1912
Linda <linda.dieckmann@rub.de>
2020-03-30 17:08:53 +0200

    added some headers

--- sudoku.py
+++ sudoku.py
@@ -1,9 +1,11 @@
 """Sudoku Game"""
+
+# import settings
 import random
 from copy import deepcopy
 import pyxel
 
-
+# functions
 def generate_random_puzzle():
     pass
 
@@ -194,13 +196,12 @@ def format_board(current_board):
             *[val if val else " " for row in current_board for val in row]
         )
 
-
 def fill_board(puzzle):
     spots = iter(puzzle)
     puzzle_board = [[int(next(spots)) for _ in range(9)] for _ in range(9)]
     return puzzle_board # change from a string to a list of list of ints
 
-
+# start script
 game_won = False
 ## Read sudoku data
 f = open("sudoku.csv")
